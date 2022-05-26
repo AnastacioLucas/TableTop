79c45f868d4e4a87c1b7f63290dfd89525427b30
Anastácio Lucas de Freitas <a.lucas_ti@hotmail.com>
2022-05-25 20:29:50 -0400

    Finished the splash screen but without motionLayout

--- app/src/main/java/com/udacity/tabletop/view/SplashScreenActivity.kt
+++ app/src/main/java/com/udacity/tabletop/view/SplashScreenActivity.kt
@@ -1,7 +1,11 @@
 package com.udacity.tabletop.view
 
+import android.app.ActivityOptions.makeSceneTransitionAnimation
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.tabletop.R
 
@@ -10,10 +14,14 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
-        // Send user to MainActivity as soon as this activity loads
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        // remove this activity from the stack
-        finish()
+
+        val backgroundImg : ImageView = findViewById(R.id.iv_logo)
+        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
+        backgroundImg.startAnimation(sideAnimation)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent, makeSceneTransitionAnimation(this).toBundle())
+            finishAfterTransition()
+        },2000)
     }
 }
